42d6fcaf38fa82af1bd731e61e2a980042dec637
benbenlaw <benlawrenceben@hotmail.co.uk>
2025-08-23 09:48:28 +0100

    1.5.6

--- src/main/java/com/benbenlaw/inworldrecipes/integration/jei/BlockInteractionRecipeCategory.java
+++ src/main/java/com/benbenlaw/inworldrecipes/integration/jei/BlockInteractionRecipeCategory.java
@@ -29,6 +29,7 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.BlockRenderDispatcher;
 import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
@@ -37,8 +38,10 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.crafting.RecipeHolder;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.LiquidBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.world.level.material.Fluid;
 import net.neoforged.neoforge.client.model.data.ModelData;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -140,9 +143,16 @@ public class BlockInteractionRecipeCategory implements IRecipeCategory<BlockInte
                             }
                         });
 
+
         Block outputBlock = recipe.outputBlockState() != null && recipe.outputBlockState().getBlock() != Blocks.AIR
                 ? recipe.outputBlockState().getBlock()
                 : (!recipe.chanceResults().isEmpty() ? Blocks.CHEST : Blocks.BARRIER);
+
+        if (outputBlock instanceof LiquidBlock liquidBlock) {
+            Fluid fluid = liquidBlock.fluid;
+            builder.addSlot(RecipeIngredientRole.OUTPUT, 120, 2)
+                    .addFluidStack(fluid);
+        }
 
         builder.addSlot(RecipeIngredientRole.OUTPUT, 120, 2)
                 .addItemStack(new ItemStack(outputBlock))
@@ -212,7 +222,6 @@ public class BlockInteractionRecipeCategory implements IRecipeCategory<BlockInte
 
 
                 });
-
 
 
         builder.addSlot(RecipeIngredientRole.INPUT, 76, 2).addIngredients(recipe.heldItem().ingredient());
